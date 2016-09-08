ad71820b2fb3a202a428e4af6ff593b87fa98b50
Nick Johnson <arachnid@notdot.net>
2016-09-08 12:50:05 +0100

    Added verification tool

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,68 @@ jQuery(window).load(function() {
     $(".loader").delay(400).fadeOut("slow");
 });
 
+var depositInterface = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "totalPaidOut",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "depositCount",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "paidOut",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "addr",
+        "type": "address"
+      }
+    ],
+    "name": "check",
+    "outputs": [
+      {
+        "name": "expires",
+        "type": "uint256"
+      },
+      {
+        "name": "deposit",
+        "type": "uint256"
+      }
+    ],
+    "type": "function"
+  }
+];
+var depositContractAddress = '0xCD6608b1291d4307652592c29bFF7d51f1AD83d7';
+var providerURL = 'https://mainnet.infura.io/Rg6BrBl8vIqJBc7AlL9h';
+
 $(document).ready(function() {
 
 	// --------------------------------------------------------
@@ -17,12 +79,6 @@ $(document).ready(function() {
 			$(".navbar").removeClass("scroll-fixed-navbar");
 		}
 	});	
-	
-	// --------------------------------------------------------
-	// Pretty Photo for Lightbox Image
-	// -------------------------------------------------------- 
-    $("a[data-gal^='prettyPhoto']").prettyPhoto(); 
-	
 	
 	// --------------------------------------------------------
 	//	Smooth Scrolling
@@ -55,6 +111,46 @@ $(document).ready(function() {
 		return false;
 	});
 
+	// --------------------------------------------------------
+	//	Verify button
+	// -------------------------------------------------------- 	
+	$('#checkbutton').click(function() {
+		if(web3 == undefined) {
+			var web3 = new Web3();
+			web3.setProvider(new web3.providers.HttpProvider(providerURL));
+		}
+
+		var address = $('#checkaddress').val();
+
+		function callback(err, result) {
+			if(err != null) {
+				$('#checkresult').html('<h3><i class="ion-close-circled"></i> Error</h3><p>Sorry, an error occurred looking up the address: ' + err + '</p>');
+			} else {
+				var timestamp = result[0].toNumber();
+				if(timestamp == 0) {
+					// Not valid
+					$('#checkresult').html("<h3><i class=\"ion-close-circled\"></i> Invalid Address</h3><p>Sorry, that doesn't appear to be the address of a genuine ether.card. If you believe it should be, please <a href=\"mailto:cards@arachnidlabs.com\">contact us</a> with a picture of the card.</p>");
+				} else {
+					var expires = new Date(timestamp * 1000);
+					var value = web3.fromWei(result[1]);
+					if(expires < new Date()) {
+						// Expired
+						$('#checkresult').html("<h3><i class=\"ion-checkmark-circled\"></i> Valid Card</h3><p>Yes! This is a valid Ether Card. However, its deposit and guarantee expired on <b>" + expires.toDateString() + "</b>.</p>");
+					} else {
+						// Valid
+						$('#checkresult').html("<h3><i class=\"ion-checkmark-circled\"></i> Valid Card</h3><p>Yes! This is a valid Ether Card. A total of <b>" + value + " ether</b> was deposited into the guarantee account for it. The guarantee expires <b>" + expires.toDateString() + "</b>.</p>");
+					}
+				}
+			}
+		}
+
+		try {
+			var contract = web3.eth.contract(depositInterface).at(depositContractAddress);
+			contract.check(address, callback);
+		} catch(e) {
+			callback(e, null);
+		} 
+	})
 });
 
 // --------------------------------------------------------
